b4a49739b727ad590fbb15b4a08f7777f045ab45
loxiang0119 <joe123441152@gmail.com>
2023-03-07 10:57:17 +0800

    加入專案檔案。

---
+++ 20230307practice/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * 這是我用來練習如何利用C#計算BMI程式範例
+ * 作者:羅竣祥
+ * 日期:2023/03/07
+ */
+namespace _20230307practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //下面這行是用來顯示文字
+            Console.Write("BMI:");
+            //底下用來計算BMI，公式:BMI = weight / (height*height)
+            Console.WriteLine(Math.Round(83 / (1.7*1.7),2));
+            Console.WriteLine(83 / Math.Pow(1.7,2));
+        }
+    }
+}
